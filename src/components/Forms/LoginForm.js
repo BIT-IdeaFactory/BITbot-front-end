182b7a85552409f8e70f27c080b0910eb7847a1d
Piotr Moszkowicz <piotr@moszkowicz.pl>
2018-11-15 21:45:23 +0100

    Fix not showing the logo

--- src/components/Forms/LoginForm.js
+++ src/components/Forms/LoginForm.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { Form, Icon, Input, Button, Checkbox, Row } from "antd";
 import Logo from "../Utils/Logo";
 import Center from "../Utils/Center";
+
 const FormItem = Form.Item;
 
 class LoginForm extends Component {
@@ -21,75 +22,77 @@ class LoginForm extends Component {
     const { form } = this.props;
     const { getFieldDecorator } = form;
     return (
-      <Center>
+      <div>
         <Logo />
-        <Form
-          onSubmit={this.handleSubmit}
-          className="login-form"
-          style={{
-            maxWidth: "300px",
-            padding: "30px",
-            paddingBottom: 0,
-            border: "1px solid lightgray",
-            borderRadius: "7px"
-          }}
-        >
-          <FormItem>
-            {getFieldDecorator("userName", {
-              rules: [{ required: true, message: "Podaj login" }]
-            })(
-              <Input
-                prefix={
-                  <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
-                }
-                placeholder="Login"
-              />
-            )}
-          </FormItem>
-          <FormItem>
-            {getFieldDecorator("password", {
-              rules: [{ required: true, message: "Podaj hasło" }]
-            })(
-              <Input
-                prefix={
-                  <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
-                }
-                type="password"
-                placeholder="Hasło"
-              />
-            )}
-          </FormItem>
-          <FormItem>
-            {getFieldDecorator("remember", {
-              valuePropName: "checked",
-              initialValue: true
-            })(<Checkbox>Zapamiętaj mnie</Checkbox>)}
-            <a className="login-form-forgot" href="/">
-              Zapomnij hasło
-            </a>
-            <Row
-              type="flex"
-              align="middle"
-              style={{
-                marginTop: "15px",
-                justifyContent: "center",
-                alignItems: "center"
-              }}
-            >
-              <Button
-                type="primary"
-                htmlType="submit"
-                className="login-form-button"
+        <Center>
+          <Form
+            onSubmit={this.handleSubmit}
+            className="login-form"
+            style={{
+              maxWidth: "300px",
+              padding: "30px",
+              paddingBottom: 0,
+              border: "1px solid lightgray",
+              borderRadius: "7px"
+            }}
+          >
+            <FormItem>
+              {getFieldDecorator("userName", {
+                rules: [{ required: true, message: "Podaj login" }]
+              })(
+                <Input
+                  prefix={
+                    <Icon type="user" style={{ color: "rgba(0,0,0,.25)" }} />
+                  }
+                  placeholder="Login"
+                />
+              )}
+            </FormItem>
+            <FormItem>
+              {getFieldDecorator("password", {
+                rules: [{ required: true, message: "Podaj hasło" }]
+              })(
+                <Input
+                  prefix={
+                    <Icon type="lock" style={{ color: "rgba(0,0,0,.25)" }} />
+                  }
+                  type="password"
+                  placeholder="Hasło"
+                />
+              )}
+            </FormItem>
+            <FormItem>
+              {getFieldDecorator("remember", {
+                valuePropName: "checked",
+                initialValue: true
+              })(<Checkbox>Zapamiętaj mnie</Checkbox>)}
+              <a className="login-form-forgot" href="/">
+                Zapomnij hasło
+              </a>
+              <Row
+                type="flex"
+                align="middle"
                 style={{
-                  width: "150px"
+                  marginTop: "15px",
+                  justifyContent: "center",
+                  alignItems: "center"
                 }}
               >
-                Zaloguj się
-              </Button>
-            </Row>
-          </FormItem>
-        </Form>
-      </Center>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  className="login-form-button"
+                  style={{
+                    width: "150px"
+                  }}
+                >
+                  Zaloguj się
+                </Button>
+              </Row>
+            </FormItem>
+          </Form>
+        </Center>
+      </div>
     );
   }
 }
@@ -113,4 +116,5 @@ LoginForm.defaultProps = {
 };
 
 const WrappedLoginForm = Form.create()(LoginForm);
+
 export default WrappedLoginForm;
