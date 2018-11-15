2d6889ba59aa45400a73e9f1d43ab32e1cfe922b
Kacper Kapuściak <kapusciak.k@gmail.com>
2018-11-15 20:35:15 +0100

    Add Center component

---
+++ src/components/Utils/Center.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Center = styled.div`
+  height: 50vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+export default Center;
