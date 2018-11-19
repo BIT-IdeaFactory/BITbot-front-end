0d6507e6324e847188cb0da4c1d6e05bff36c1d4
Piotr Moszkowicz <piotr@moszkowicz.pl>
2018-11-19 14:12:16 +0100

    Changed way we center things

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,8 +1,18 @@
 import React from "react";
 import { withRouter } from "react-router";
 
+import { Col, Row } from "antd";
+
+import Logo from "../components/Utils/Logo";
 import WrappedLoginForm from "../components/Forms/LoginForm";
 
-const LoginPage = () => <WrappedLoginForm />;
+const LoginPage = () => (
+  <Row type="flex" justify="center" align="middle" style={{ height: "100%" }}>
+    <Col>
+      <Logo />
+      <WrappedLoginForm />
+    </Col>
+  </Row>
+);
 
 export default withRouter(LoginPage);
